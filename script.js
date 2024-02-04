be3b8e7bf17d73d419378c55434ee3178baf8bdc
Kunal Pathe <157122408+Kunal-Pathe23@users.noreply.github.com>
2024-02-04 20:50:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function generateBackground() {
     var color1 = document.getElementById('color1').value;
-    // var color2 = document.getElementById('color2').value;
-    // document.body.style.background = 'linear-gradient(to right, ' + color1 + ', ' + color2 + ')';
     document.body.style.background = color1;
 }
